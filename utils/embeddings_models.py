43bd6dfcaffc5c7d0d1577c8d783e712f695bf84
GetachewAbebe <gechachin@gmail.com>
2025-04-05 04:55:01 +0300

    Updates

--- utils/embeddings_models.py
+++
@@ -1,5 +0,0 @@
-from langchain_community.embeddings import OpenAIEmbeddings
-
-def get_embeddings_model():
-    """Initialize and return the OpenAI embeddings model."""
-    return OpenAIEmbeddings()
